9116a1342f7328ec6fb6de700db064035d575eec
Quoc_Thang <112066044+taquocthangg@users.noreply.github.com>
2023-09-12 02:05:25 +0700

    Update hoaDonController.js

--- controllers/hoaDonController.js
+++ controllers/hoaDonController.js
@@ -33,15 +33,7 @@ const hoaDonController = {
   },
 
   //UPDATE hoaDon
-  updatehoaDon: async (req, res) => {
-    try {
-      const hoaDon = await hoaDon.findById(req.params.id);
-      await hoaDon.updateOne({ $set: req.body });
-      res.status(200).json("Updated successfully!");
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+
 
   //DELETE hoaDon
   
